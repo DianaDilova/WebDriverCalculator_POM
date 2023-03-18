11acbb5e6b245e47117df4e30c4ed4fe50331af8
Diana Dilova <dianadilovaa@gmail.com>
2023-03-18 17:56:24 +0200

    Add project files.

---
+++ Tests/SumNumbersPageTests.cs
@@ -0,0 +1,60 @@
+using WebDriverCalculatorPOM.Pages;
+
+namespace WebDriverCalculatorPOM.Tests
+{
+    public class SumNumbersPageTests : BaseTest
+    {
+        private SumNumbersPage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            page = new SumNumbersPage(driver);
+            page.Open();
+        }
+
+        [Test]
+        public void Test_SumNumberPage_CheckTitle()
+        {
+            Assert.That(page.GetPageTitle(), Is.EqualTo("Number Calculator"));
+        }
+
+        [Test]
+        public void Test_SumNumberPage_SumTwoPositiveNumbers()
+        {
+            page.Open();
+            var actualResult = page.CalculateNumbers("5", "+", "15");
+            Assert.That(actualResult, Is.EqualTo("Result: 20"));
+
+        }
+
+        [Test]
+        public void Test_SumNumberPage_MultiplyTwoPositiveNumbers()
+        {
+            page.Open();
+            var actualResult = page.CalculateNumbers("5", "*", "15");
+            Assert.That(actualResult, Is.EqualTo("Result: 75"));
+
+        }
+
+        [TestCase("5", "*", "15", "Result: 75")]
+        [TestCase("5", "+", "15", "Result: 20")]
+        [TestCase("5", "-", "15", "Result: -10")]
+        [TestCase("15", "/", "3", "Result: 5")]
+
+        public void Test_SumNumberPage_MultiplyTwoPositiveNumbers(
+            string firstValue, string operation, string secondValue, string result)
+        {
+            var actualResult = page.CalculateNumbers(firstValue, operation, secondValue);
+            Assert.That(actualResult, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Test_SumNumberPage_ResetButton()
+        {
+            page.CalculateNumbers("6", "+", "8");
+            page.ResetButton.Click();
+            Assert.True(page.IsFormEmpty());
+        }
+    }
+}
